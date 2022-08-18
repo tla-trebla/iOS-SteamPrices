47c1aa058c317f3718e3e0e02c4a5e0b600cffea
Albert Pangestu <stamina.left@gmail.com>
2022-08-18 14:47:03 +0700

    Removed `Main.storyboard` Setup `SceneDelegate` for no Storyboard & removed unused function Improved folder tidiness

---
+++ SteamPrices/Main/SceneDelegate.swift
@@ -0,0 +1,24 @@
+//
+//  SceneDelegate.swift
+//  SteamPrices
+//
+//  Created by Albert Pangestu on 18/08/22.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        
+        window = UIWindow(frame: windowScene.coordinateSpace.bounds)
+        window?.windowScene = windowScene
+        window?.rootViewController = ViewController()
+        window?.makeKeyAndVisible()
+    }
+}
+
